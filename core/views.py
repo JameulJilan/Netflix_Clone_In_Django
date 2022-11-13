f7fec5972271b8dde99e9714fab1e6ad342f1141
jameul <jameuljilan@gmail.com>
2022-11-13 18:58:52 +0900

    upto create profile template is complete

--- core/views.py
+++ core/views.py
@@ -1,9 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+
+
 
 class Home(View):
     def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('core:profile_list')            
         return render(request=request,template_name='index.html')
+
+@method_decorator(login_required,name='dispatch')
+class ProfileList(View):
+    def get(self, request, *args, **kwargs):
+        profiles = request.user.profiles.all()
+        return render(request=request,template_name='profileList.html',context={
+            'profiles':profiles
+        })
+
+class ProfileCreate(View):
+    def get(self, request, *args, **kwargs):
+        return render(request=request,template_name='profileCreate.html')
 
 
 
--- django_netflix/settings.py
+++ django_netflix/settings.py
@@ -37,7 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
     'core',
+
+     # third party apps
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
 ]
 
 MIDDLEWARE = [
@@ -131,3 +137,22 @@ STATICFILES_DIRS=[
 
 # Auth setting
 AUTH_USER_MODEL='core.CustomUser'
+
+AUTHENTICATION_BACKENDS = [
+
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+  
+]
+
+SITE_ID=1
+
+ACCOUNT_AUTHENTICATION_METHOD='email'
+ACCOUNT_EMAIL_REQUIRED=True
+ACCOUNT_EMAIL_VERIFICATION='none'
+ACCOUNT_USERNAME_REQUIRED=False
+
+LOGIN_REDIRECT_URL='/'
